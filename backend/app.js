34dcc03375c1851d7d834010b40e06d3728be45e
tonytornado <n10694218@qut.edu.au>
2025-07-03 14:07:14 +1000

    Allow requests from deployed frontend by updating CORS origin list

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5174',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'https://express-api-indol.vercel.app',
 ];
 
 app.use(express.json());
